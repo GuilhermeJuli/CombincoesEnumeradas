0259fa6678c11472d7dc0296111ed14e6268c214
Guilherme Moraes Julio <102086160+GuilhermeJuli@users.noreply.github.com>
2022-04-13 16:02:39 -0300

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,20 @@
+package com.application;
+
+import com.entities.enums.Order;
+import com.entities.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Order order = new Order(1080, new Date(), OrderStatus.PENDING_PAYMENT);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+
+        System.out.println("PAYMENTE");
+        System.out.println(order);
+    }
+}
